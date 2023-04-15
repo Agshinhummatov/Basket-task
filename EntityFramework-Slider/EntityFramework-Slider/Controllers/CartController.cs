dd43a6c7377bfb59f713970ecebc5848c46805fd
Agshinhummatov <agshinshh@code.edu.az>
2023-04-15 17:44:09 +0400

    update

---
+++ EntityFramework-Slider/EntityFramework-Slider/Controllers/CartController.cs
@@ -0,0 +1,84 @@
+﻿using EntityFramework_Slider.Data;
+using EntityFramework_Slider.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using NuGet.ContentModel;
+
+namespace EntityFramework_Slider.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CartController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult>  Index()
+        {
+
+            List<BasketVM> basketProducts;
+
+            if (Request.Cookies["basket"] != null)
+            {
+                basketProducts = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+
+            }
+            else
+            {
+                basketProducts = new List<BasketVM>();
+            }
+
+
+          
+
+
+            List<BasketDetailVM> basketDetails = new();
+
+            foreach (var product in basketProducts)
+            {
+
+                var dbProduct = await _context.Products.Include(m=>m.Images).Include(m => m.Category).FirstOrDefaultAsync(m=> m.Id == product.Id);
+
+                basketDetails.Add(new BasketDetailVM
+                {    Id = product.Id,
+                     Name = dbProduct.Name,
+                     CategoryName = dbProduct.Category.Name,
+                     Description = dbProduct.Description,
+                     Price = dbProduct.Price,
+                     Image = dbProduct.Images.Where(m=>m.IsMain).FirstOrDefault().Image,
+                     Count= product.Count,             //data bazada olmayan countu verir
+                     Total = dbProduct.Price * product.Count
+                     
+
+                });
+
+
+            }
+
+            return View(basketDetails);
+
+
+        }
+
+        [ActionName("Delete")]
+        public IActionResult DeleteProductFormBasket(int? id)
+        {
+            if (id is null) return BadRequest();
+
+            List<BasketVM> basketProducts = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+
+            BasketVM deletedProdcut = basketProducts.FirstOrDefault(m => m.Id == id);
+
+            basketProducts.Remove(deletedProdcut);
+
+
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(basketProducts));
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
